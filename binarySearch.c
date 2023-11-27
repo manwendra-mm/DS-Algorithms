719a4b337a489cd1a5981a6f32beaeffc9a1faa9
Manwendra Mahato <manwendramahato@gmail.com>
2023-11-27 23:33:13 +0530

    update binarySearch.c

--- binarySearch.c
+++ binarySearch.c
@@ -4,8 +4,70 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int arr[10] = {1, 2, 32, 43, 45, 49, 50, 67, 90, 100};
+int static arr[10] = {1, 2, 32, 43, 45, 49, 50, 67, 90, 100};
 
+int binarySearch(int a) //a is searched element
+//Func returns 1 if found and 0 if not found
+{
+    int f, r, m, val;
+    f = 0;
+    r = sizeof(arr)/sizeof(arr[0]);
+    printf("No of Elements: \n%d\n", r); //printing size of array
+    m = (f+r)/2;
+    printf("Enter the value to be Searched: ");
+    scanf("%d", &val);
+    while(f<=r){
+        if (val == arr[m])
+        {
+            return 1;
+        }
+        if (val > arr[m])
+        {
+            f = m +1;
+        }
+        else{
+            r = m -1;
+        }
+        
+    }
+    return 0;
+}
+
+
+
+int main(){
+    
+    
+    printf("The Elements are:\n");
+    for (int i =0; i< sizeof(arr)/sizeof(arr[0]); i++) //Prnting elements
+    {
+        printf("%d\n", arr[i]);
+
+    }
+
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*............    
 void display(){
     int i;
     for(i=0; i<= sizeof(arr)/sizeof(arr[0])-1; i++)
@@ -21,18 +83,22 @@ int binarySearch(int list[], int a) //a is searched element
 {
     int f, r, m;
     f = 0;
-    r = sizeof(list)/sizeof(list[0])-1;
+    r = (sizeof(list)/sizeof(list[0]))-1;
+    printf("%d", r);
     m = (f + r) / 2;
 
     while(f <= r)
     {
         if (a < list[m]){
+            printf("Test1");
             r = m-1;
         }
         else if(a > list [m]){
+            printf("Test2");
             f = m+1;
         }
         else{
+            printf("Test3");
             return 1;
         }
     }
@@ -41,34 +107,41 @@ int binarySearch(int list[], int a) //a is searched element
 }
 
 int main(){
+    int arr[10] = {1, 2, 32, 43, 45, 49, 50, 67, 90, 100};
 
-    int s, a, flag; //s if choice selected 
-    //Flag is value to be searched
-    while (1){
+    int s, val, flag = 0; //s if choice selected 
+    //Val is value to be searched
+    while(1){
         printf("1. Number of Items in List\n2. Search using Binary Search\n3. Exit\n");
         printf("Enter your Choice: ");
         scanf ("%d", &s);
-        switch(s){
+        switch(s)
+        {
             case 1:
-            printf("No. of Elements in List are: %d", sizeof(arr)/sizeof(arr[0]));
-            break;
+                printf("No. of Elements in List are: %d", sizeof(arr)/sizeof(arr[0]));
+                break;
 
             case 2:
-            printf("Enter the Value to be Searched: ");
-            scanf("%d", &a);
-            flag = binarySearch(arr, a);
-            if (flag == 1){
-                printf("Element is present in List");
-            }
-            else{
-                printf("Element was not found");
-            }
-            break;
+                printf("Enter the Value to be Searched: ");
+                scanf("%d", &val);
+                flag = binarySearch(arr, val);
+                if (flag == 1){
+                    printf("Element is present in List");
+                }
+                else{
+                    printf("Element was not found");
+                }
+                break;
             
             case 3:
-            exit(0);
-            break;
+                exit(0);
+                break;
+            
+            default:
+                continue;
+
         }
         return 0;
     }
 }
+.................*/
